ad9473bf0b8c2524ef446f4644c149dad12dcafd
Nathan Hammond <github.com@nathanhammond.com>
2016-04-18 13:32:08 -0700

    Make the router CJS compatible.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 /* jshint node: true */
 'use strict';
 
+const fs = require('fs');
+const process = require('process');
+const path = require('path');
+const rollup = require('rollup');
+
+const Ember = require(path.join(process.cwd(),'bower_components','ember','ember-runtime'));
+// const config =
+
 module.exports = {
-  name: 'ember-cli-router-precompile'
+  name: 'ember-cli-router-precompile',
+  preBuild: function() {
+    let appPath = [];
+    appPath.push(this.project.root);
+
+    // FIXME: How should I look up the actual place on disk?
+    if (true) { appPath.push('tests/dummy'); }
+
+    appPath.push(this.treePaths.app);
+
+    appPath = path.join.apply(undefined, appPath);
+
+    rollup.rollup({
+      entry: path.join(appPath, 'router.js'),
+      external: ['ember', path.join(appPath, 'config/environment')]
+    }).then(function(bundle) {
+      bundle.write({ format: 'cjs', dest: path.join(appPath,'router.cjs') });
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
 };
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('foo');
+  this.route('bar');
+  this.route('baz');
 });
 
 export default Router;
